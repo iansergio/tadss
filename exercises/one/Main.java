1ca4c4598d429be82d9d9f8bec26b8cd3874e05d
Ian Sergio Helfenberger <iansergiohelf@gmail.com>
2026-03-10 18:07:34 -0300

    first exercise

---
+++ exercises/one/Main.java
@@ -0,0 +1,17 @@
+package exercises.one;
+
+import java.util.Date;
+
+public class Main extends Thread {
+
+    public void run() {
+        System.out.println(getName() + " " + new Date());
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Main a = new Main();
+            a.start();
+        }
+    }
+}
